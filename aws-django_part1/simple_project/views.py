7341e9155c71cbacf542043639f230335e7487ac
XikoCat <guigasreis99@gmail.com>
2021-06-11 14:29:24 +0100

    cleanup3

--- aws-django_part1/simple_project/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .hello import add
-
-def function(request):
-    return render(request, "function.html", {'soma' : add(1,2)})
-
-
-
-
--- azr-django_part1/simple_project/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .hello import add
-
-def function(request):
-    return render(request, "function.html", {'soma' : add(1,2)})
-
-
-
